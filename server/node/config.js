55131435271b93cc2126bd5ee9da2c2363675faf
Egor Sadanov <egorsadanov@yandex.ru>
2020-10-28 16:02:38 +1100

    update shipping options

--- server/node/config.js
+++ server/node/config.js
@@ -1,8 +1,6 @@
 /**
  * config.js
- * Stripe Payments Demo. Created by Romain Huet (@romainhuet)
- * and Thorsten Schaeff (@thorwebdev).
- */
+  */
 
 'use strict';
 
@@ -16,57 +14,39 @@ module.exports = {
   // Store currency.
   currency: 'aud',
 
-  // Supported payment methods for the store.
-  // Some payment methods support only a subset of currencies.
-  // Make sure to check the docs: https://stripe.com/docs/sources
   paymentMethods: [
-    // 'ach_credit_transfer', // usd (ACH Credit Transfer payments must be in U.S. Dollars)
-    'alipay', // aud, cad, eur, gbp, hkd, jpy, nzd, sgd, or usd.
-    //'bancontact', // eur (Bancontact must always use Euros)
-    'card', // many (https://stripe.com/docs/currencies#presentment-currencies)
-    //'eps', // eur (EPS must always use Euros)
-    //'ideal', // eur (iDEAL must always use Euros)
-    //'giropay', // eur (Giropay must always use Euros)
-    //'multibanco', // eur (Multibanco must always use Euros)
-    // 'sepa_debit', // Restricted. See docs for activation details: https://stripe.com/docs/sources/sepa-debit
-   // 'p24', // eur, pln
-    //'sofort', // eur (SOFORT must always use Euros)
-    'wechat', // aud, cad, eur, gbp, hkd, jpy, sgd, or usd.
+    'card'
   ],
 
   // Configuration for Stripe.
-  // API Keys: https://dashboard.stripe.com/account/apikeys
-  // Webhooks: https://dashboard.stripe.com/account/webhooks
-  // Storing these keys and secrets as environment variables is a good practice.
-  // You can fill them in your own `.env` file.
   stripe: {
-    // The two-letter country code of your Stripe account (required for Payment Request).
     country: process.env.STRIPE_ACCOUNT_COUNTRY || 'AU',
-    // API version to set for this app (Stripe otherwise uses your default account version).
     apiVersion: '2019-03-14',
-    // Use your test keys for development and live keys for real charges in production.
-    // For non-card payments like iDEAL, live keys will redirect to real banking sites.
     publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
     secretKey: process.env.STRIPE_SECRET_KEY,
-    // Setting the webhook secret is good practice in order to verify signatures.
-    // After creating a webhook, click to reveal details and find your signing secret.
     webhookSecret: process.env.STRIPE_WEBHOOK_SECRET,
   },
 
   // Shipping options for the Payment Request API.
   shippingOptions: [
     {
-      id: 'free',
-      label: 'Free Shipping',
-      detail: 'Delivery according selected date',
-      amount: 0,
+      id: 'standard',
+      label: 'Standard Shipping',
+      detail: 'Weekdays and Weekends',
+      amount: 2500,
     },
     {
-      id: 'weekend',
-      label: 'Weekend Shipping',
-      detail: 'Next available weekend delivery',
-      amount: 500,
+      id: 'remote',
+      label: 'Remote Suburbs Shipping',
+      detail: 'Weekdays and Weekends',
+      amount: 3500,
     },
+    {
+      id: 'cbd',
+      label: 'CBD Shipping',
+      detail: 'Weekdays and Weekends',
+      amount: 5000,
+    }
   ],
 
   // Server port.
