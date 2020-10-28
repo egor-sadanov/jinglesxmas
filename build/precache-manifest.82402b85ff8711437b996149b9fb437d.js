cdd3f56b624a7a2bbdad494ec4bfd86da5ae34f8
Alex <thesoftbytes@gmail.com>
2020-10-28 13:12:52 +1100

    comitting local updates3

---
+++ build/precache-manifest.82402b85ff8711437b996149b9fb437d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe0857788f4e44a610685937a747934",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a95b0490c4fd463d6a31",
+    "url": "/static/css/2.abb78336.chunk.css"
+  },
+  {
+    "revision": "4ee5cca132842e65e5a8",
+    "url": "/static/css/main.00cda3fa.chunk.css"
+  },
+  {
+    "revision": "a95b0490c4fd463d6a31",
+    "url": "/static/js/2.6140008a.chunk.js"
+  },
+  {
+    "revision": "4ee5cca132842e65e5a8",
+    "url": "/static/js/main.39d42574.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
